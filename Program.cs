4d87a03079d20b4c8069118f48b9965efb198419
Jeter807 <156726547+Jeter807@users.noreply.github.com>
2024-02-04 14:11:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Lab6cs
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter value for 'a' and press enter: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter value for 'b' and press enter: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate and display the average of the two numbers
+        double average = (a + b) / 2;
+        Console.WriteLine($"The average of {a} and {b} is: {average}");
+
+        // Ask the user for an additional number
+        Console.Write("Enter an additional number: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate and display the average of all three numbers
+        double totalAverage = (a + b + c) / 3;
+        Console.WriteLine($"The average of {a}, {b}, and {c} is: {totalAverage}");
+    }
+}
+
